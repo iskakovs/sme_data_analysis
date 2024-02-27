23c516b834e6858a4c18df73d0fb33c9829d302d
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-02-27 15:21:50 +0600

    Update smes.R
    Plot ACF and PACF of the residuals

--- smes.R
+++ smes.R
@@ -280,3 +280,6 @@ print(break_summary)
 
 # Calculate residuals from the model
 residuals_nw <- residuals(ols_model_adj_nw)
+
+# Plot ACF and PACF of the residuals
+acf(residuals_nw, main = "ACF of Residuals")
